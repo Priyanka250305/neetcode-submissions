c76267e0d88ba41b0444aa941871562cd5f3dd0e
Priyanka soni <161323407+Priyanka250305@users.noreply.github.com>
2026-06-09 16:15:57 +0530

    Add: missing-number - submission-0

---
+++ Data Structures & Algorithms/missing-number/submission-0.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int counter=nums[0];
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i]==counter){counter++;}
+            else{ return counter;}
+        }
+        return 0;
+    }
+};
